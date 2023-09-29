9b65f8ddb85ac14083f41bb89204de4b32b8d774
Murtaza Subhani <ms227sb@student.lnu.se>
2023-09-29 20:33:33 +0200

    UI

--- app/src/main/java/view/UI.java
+++ app/src/main/java/view/UI.java
@@ -4,17 +4,91 @@ import java.util.Scanner;
 
 public class UI {
     private Scanner scanner;
-    
+
     public UI() {
         scanner = new Scanner(System.in);
     }
 
-
     public void displayMenu() {
-        System.out.println("Welcome to the Stuff Lending System! ");
-            
+        System.out.println("Welcome to the Stuff Lending System!");
 
+        while (true) {
+            System.out.println("\nMain Menu:");
+            System.out.println("1. Member Management");
+            System.out.println("2. Item Management");
+            System.out.println("3. Contract Management");
+            System.out.println("4. Advance Time");
+            System.out.println("5. Exit");
 
-        scanner.close();
+            System.out.print("\nEnter your choice: ");
+
+            int choice = getIntInput();
+
+            switch (choice) {
+                case 1:
+                    // Call a method to handle Member Management
+                    handleMemberManagement();
+                    break;
+
+                case 2:
+                    // Call a method to handle Item Management
+                    handleItemManagement();
+                    break;
+
+                case 3:
+                    // Call a method to handle Contract Management
+                    handleContractManagement();
+                    break;
+
+                case 4:
+                    // Call a method to handle advancing time
+                    advanceTime();
+                    break;
+
+                case 5:
+                    System.out.println("Exiting the application.");
+                    scanner.close(); // Close the scanner before exiting
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Choice. Please enter a valid option.");
+            }
+        }
+    }
+
+    private int getIntInput() {
+        int input = -1;
+        try {
+            input = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            // Invalid input; do nothing, the loop will prompt again.
+        }
+        return input;
+    }
+
+    // methods to handle different functionalities
+    private void handleMemberManagement() {
+        System.out.println("Member Management:");
+        // member management logic
+    }
+
+    private void handleItemManagement() {
+        System.out.println("Item Management:");
+        // item management logic
+    }
+
+    private void handleContractManagement() {
+        System.out.println("Contract Management:");
+        // contract management logic
+    }
+
+    private void advanceTime() {
+        System.out.println("Advancing Time:");
+        // time advancement logic
+    }
+
+    public static void main(String[] args) {
+        UI mainMenu = new UI();
+        mainMenu.displayMenu();
     }
 }
