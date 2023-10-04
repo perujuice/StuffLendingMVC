c7b31ca023fda9118c8e79c30ffd192f169b7316
Marteinn <mh226eh@student.lnu.se>
2023-10-04 19:10:39 +0200

    small fix

--- app/src/main/java/view/UI.java
+++ app/src/main/java/view/UI.java
@@ -13,152 +13,152 @@ public class UI {
   MemberController memberController = new MemberController();
   ItemController itemController = new ItemController();
 
-    public UI() {
-        scanner = new Scanner(System.in);
-    }
+  public UI() {
+      scanner = new Scanner(System.in);
+  }
 
-    public void displayMenu() {
-        System.out.println("Welcome to the Stuff Lending System!");
+  public void displayMenu() {
+    System.out.println("Welcome to the Stuff Lending System!");
 
-        while (true) {
-            System.out.println("\nMain Menu:");
-            System.out.println("1. Member Management");
-            System.out.println("2. Item Management");
-            System.out.println("3. Contract Management");
-            System.out.println("4. Advance Time");
-            System.out.println("5. Exit");
+      while (true) {
+          System.out.println("\nMain Menu:");
+          System.out.println("1. Member Management");
+          System.out.println("2. Item Management");
+          System.out.println("3. Contract Management");
+          System.out.println("4. Advance Time");
+          System.out.println("5. Exit");
 
-            System.out.print("\nEnter your choice: ");
+          System.out.print("\nEnter your choice: ");
 
-            int choice = getIntInput();
+          int choice = getIntInput();
 
-            switch (choice) {
-                case 1:
-                    // Member Management
-                    handleMemberManagement();
-                    break;
-                case 2:
-                    // Call a method to handle Item Management
-                    handleItemManagement();
-                    break;
-                case 3:
-                    // Call a method to handle Contract Management
-                    handleContractManagement();
-                    break;
-                case 4:
-                    // Call a method to handle advancing time
-                    advanceTime();
-                    break;
-                case 5:
-                    System.out.println("Exiting the application.");
-                    scanner.close(); // Close the scanner before exiting
-                    System.exit(0);
+          switch (choice) {
+              case 1:
+                  // Member Management
+                  handleMemberManagement();
+                  break;
+              case 2:
+                  // Call a method to handle Item Management
+                  handleItemManagement();
+                  break;
+              case 3:
+                  // Call a method to handle Contract Management
+                  handleContractManagement();
+                  break;
+              case 4:
+                  // Call a method to handle advancing time
+                  advanceTime();
+                  break;
+              case 5:
+                  System.out.println("Exiting the application.");
+                  scanner.close(); // Close the scanner before exiting
+                  System.exit(0);
 
-                default:
-                    System.out.println("Invalid Choice. Please enter a valid option.");
-            }
-        }
-    }
-
-    private int getIntInput() {
-        int input = -1;
-        try {
-            input = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            // Invalid input; do nothing, the loop will prompt again.
-        }
-        return input;
-    }
-
-    // methods to handle different functionalities
-    private void handleMemberManagement() {
-        System.out.println("Member Management:");
-        // member management logic
-        while (true) {
-            System.out.println("\nMember Management Menu:");
-            System.out.println("1. Create member");
-            System.out.println("2. Delete member");
-            System.out.println("3. View member information");
-            System.out.println("4. Change member information");
-            System.out.println("5. List all memebers");
-            System.out.println("6. Back");
-
-            System.out.print("\nEnter your choice: ");
-
-            int choice = getIntInput();
-
-            switch (choice) {
-                case 1:
-                    // Create Member.
-                    System.out.println("Creating a new member...\n");
-                    System.out.print("Enter the member's name: ");
-                    String name = scanner.nextLine();
-
-                    System.out.print("Enter the member's email: ");
-                    String email = scanner.nextLine();
-
-                    System.out.print("Enter the member's phone number: ");
-                    int phoneNr = scanner.nextInt();
-
-                    scanner.nextLine();
-                    // creat instance create a new member
-                    // MemberController memberController = new MemberController();
-                    Member newMember = memberController.createMember(name, email, phoneNr);
-
-                    // Print a confirmatiom message
-                    System.out.println("New member created with Member ID: " + newMember.getMemberId());
-                    break;
-
-                case 2:
-                    // Deletes a member by member ID
-                    System.out.print("Enter the member's ID to be deleted: ");
-                    String mDeleteID = scanner.nextLine();
-                    memberController.deleteMember(mDeleteID);
-                    System.out.println("Member successfully deleted! ");
-                    break;
-
-
-        case 3:
-          // View member information
-          //  This probably has to be updated to show more information!
-          System.out.print("Enter the member's ID to view information: ");
-          String mViewID = scanner.nextLine();
-          memberController.printMemberInfo(mViewID);
-        
-        case 4:
-          //Change member information by member ID
-          changeMemberInfo();
-          break;
-        case 5:
-          //list all members
-          List<String> allMemberNames = memberController.getAllMemberNames();
-          
-          // Now 'allMemberNames' contains all member names
-          for (String member_name : allMemberNames) {
-              // Display the member name as needed
-              System.out.println("Name: " + member_name);
+              default:
+                  System.out.println("Invalid Choice. Please enter a valid option.");
           }
-          break;
+      }
+  }
 
-                case 6:
-                    displayMenu();
-                    break;
-            }
+  private int getIntInput() {
+      int input = -1;
+      try {
+          input = Integer.parseInt(scanner.nextLine());
+      } catch (NumberFormatException e) {
+          // Invalid input; do nothing, the loop will prompt again.
+      }
+      return input;
+  }
+
+  // methods to handle different functionalities
+  private void handleMemberManagement() {
+      System.out.println("Member Management:");
+      // member management logic
+      while (true) {
+          System.out.println("\nMember Management Menu:");
+          System.out.println("1. Create member");
+          System.out.println("2. Delete member");
+          System.out.println("3. View member information");
+          System.out.println("4. Change member information");
+          System.out.println("5. List all memebers");
+          System.out.println("6. Back");
+
+          System.out.print("\nEnter your choice: ");
+
+          int choice = getIntInput();
+
+          switch (choice) {
+              case 1:
+                  // Create Member.
+                  System.out.println("Creating a new member...\n");
+                  System.out.print("Enter the member's name: ");
+                  String name = scanner.nextLine();
+
+                  System.out.print("Enter the member's email: ");
+                  String email = scanner.nextLine();
+
+                  System.out.print("Enter the member's phone number: ");
+                  int phoneNr = scanner.nextInt();
+
+                  scanner.nextLine();
+                  // creat instance create a new member
+                  // MemberController memberController = new MemberController();
+                  Member newMember = memberController.createMember(name, email, phoneNr);
+
+                  // Print a confirmatiom message
+                  System.out.println("New member created with Member ID: " + newMember.getMemberId());
+                  break;
+
+              case 2:
+                  // Deletes a member by member ID
+                  System.out.print("Enter the member's ID to be deleted: ");
+                  String mDeleteID = scanner.nextLine();
+                  memberController.deleteMember(mDeleteID);
+                  System.out.println("Member successfully deleted! ");
+                  break;
+
+
+      case 3:
+        // View member information
+        //  This probably has to be updated to show more information!
+        System.out.print("Enter the member's ID to view information: ");
+        String mViewID = scanner.nextLine();
+        memberController.printMemberInfo(mViewID);
+      
+      case 4:
+        //Change member information by member ID
+        changeMemberInfo();
+        break;
+      case 5:
+        //list all members
+        List<String> allMemberNames = memberController.getAllMemberNames();
+        
+        // Now 'allMemberNames' contains all member names
+        for (String member_name : allMemberNames) {
+            // Display the member name as needed
+            System.out.println("Name: " + member_name);
         }
-    }
+        break;
 
-    private void handleItemManagement() {
-        System.out.println("Item Management:");
-        // item management logic
-        while (true) {
-            System.out.println("\nItem Management Menu:");
-            System.out.println("1. Create Item");
-            System.out.println("2. List all Items");
-            System.out.println("3. Back");
+              case 6:
+                  displayMenu();
+                  break;
+          }
+      }
+  }
 
-            System.out.print("\nEnter your choice: ");
+  private void handleItemManagement() {
+    System.out.println("Item Management:");
+    // item management logic
+    while (true) {
+      System.out.println("\nItem Management Menu:");
+      System.out.println("1. Create Item");
+      System.out.println("2. List all Items");
+      System.out.println("3. Back");
 
-            int choice = getIntInput();
+      System.out.print("\nEnter your choice: ");
+
+      int choice = getIntInput();
 
       switch (choice) {
         case 1:
@@ -194,12 +194,6 @@ public class UI {
         case 2:
           
       }
-  }
-
-}
-
-      }
-    
     }
 
   }
