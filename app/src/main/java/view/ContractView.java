f9705b531cb182c9730b23e955a7d481cf0ac886
Marteinn <mh226eh@student.lnu.se>
2023-11-16 04:18:26 +0100

    view is passive now, not depending on controller.

--- app/src/main/java/view/ContractView.java
+++ app/src/main/java/view/ContractView.java
@@ -1,93 +1,65 @@
 package view;
 
-import controller.ContractController;
-import controller.ItemController;
-import controller.MemberController;
+import java.util.List;
 import java.util.Scanner;
 import model.Contract;
-import model.Item;
-import model.Member;
+import model.ContractRegistry;
 
 /**
  * Class for contract UI stuff.
  */
 public class ContractView {
   private Scanner scanner;
-  private ItemController itemController;
-  private MemberController memberController;
-  private ContractController contractController;
+  private ContractRegistry contractRegistry;
 
-  /**
-   * Constructor for contract management.
 
-   * @param originalController Pass in the member controller.
-   * @param itemController Pass in the item controller.
-   */
-  public ContractView(MemberController originalController,
-      ItemController itemController, ContractController contractController) {
-    this.scanner = new Scanner(System.in, "UTF-8");
-    this.itemController = itemController;
-    this.memberController = originalController;
-    this.contractController = contractController;
+  public void displayContractMenu() {
+    System.out.println("\nContract Management Menu:");
+    System.out.println("1. Create a new lending contract");
+    System.out.println("2. List all contracts");
+    System.out.println("3. Back");
 
+    System.out.print("\nEnter your choice: ");
+  }
+
+  public String promptBorrowerId() {
+    System.out.println("Creating a new contract...\n");
+    System.out.print("Enter member ID of the Borrower: ");
+    String borrowerId = scanner.nextLine();
+    return borrowerId;
+  }
+
+  public String promptItemId() {
+    System.out.print("Enter Item ID of the item to borrow: ");
+    String itemId = scanner.nextLine();
+    return itemId;
+  }
+
+  public int promptReturnDay() {
+    System.out.print("Enter the day of return: ");
+    int endDate = getIntInput();
+    return endDate;
   }
 
   /**
-   * Handles the contract management user interface.
+   * Method to list all contracts.
    */
-  public void handleContractManagement() {
-    System.out.println("Contract Management:");
+  public void listAllContracts() {
+    List<Contract> contracts = contractRegistry.getAllContracts();
 
-    while (true) {
-      System.out.println("\nContract Management Menu:");
-      System.out.println("1. Create a new lending contract");
-      System.out.println("2. List all contracts");
-      System.out.println("3. Back");
-
-      System.out.print("\nEnter your choice: ");
-
-      int choice = getIntInput();
-      switch (choice) {
-        case 1:
-          // Create Contract.
-          System.out.println("Creating a new contract...\n");
-          System.out.print("Enter member ID of the Borrower: ");
-          String borrowerMemberId = scanner.nextLine();
-          Member borrower = memberController.searchMember(borrowerMemberId);
-
-          if (borrower != null) {
-            System.out.print("Enter Item ID of the item to borrow: ");
-            final String itemId = scanner.nextLine();
-
-            System.out.print("Enter the day of return: ");
-            final int endDate = getIntInput();
-
-            Item item = itemController.searchItem(itemId);
-            if (item != null) {
-              Member lender = item.getOwner();
-
-              Contract newContract = contractController.createContract(borrower, lender, item, endDate);
-              if (newContract != null) {
-                System.out.println("Contract created successfully. Contract ID: " + newContract.getContractId());
-              } else {
-                System.out.println(
-                    "Failed to create a contract."
-                      + "Check if the lender has enough credits or the item is not available.");
-              }
-            } else {
-              System.out.println("Item not found with the specified item ID.");
-            }
-          } else {
-            System.out.println("Borrower not found with the specified member ID.");
-          }
-          break;
-        case 2:
-          contractController.listAllContracts();
-          break;
-        case 3:
-          return; // Go back to the main menu.
-        default:
-          System.out.println("Invalid choice. Please try again.");
+    if (contracts.isEmpty()) {
+      System.out.println("No contracts found.");
+    } else {
+      System.out.println("\nList of all contracts:");
+      for (Contract contract : contracts) {
+        System.out.println("Contract ID: " + contract.getContractId());
+        System.out.println("Lender: " + contract.getLender().getName());
+        System.out.println("Borrower: " + contract.getBorrower().getName());
+        System.out.println("Item: " + contract.getItem().getName());
+        System.out.println("Start Date: " + contract.getStartDate());
+        System.out.println("End Date: " + contract.getEndDate());
+        System.out.println("Total Cost: " + contract.getTotalCost());
+        System.out.println();
       }
     }
   }
--- app/src/main/java/view/ItemView.java
+++ app/src/main/java/view/ItemView.java
@@ -1,32 +1,21 @@
 package view;
 
-import controller.ItemController;
-import controller.MemberController;
 import java.util.List;
 import java.util.Scanner;
 import model.Contract;
 import model.Item;
 import model.ItemCategory;
+import model.ItemRegistry;
 import model.Member;
+import model.MemberRegistry;
 
 /**
  * Class for Item UI.
  */
 public class ItemView {
   private Scanner scanner;
-  private ItemController itemController;
-  private MemberController memberController;
-
-  /**
-   * Construcor for item ui.
-
-   * @param originalController Member controller instance passed in.
-   */
-  public ItemView(MemberController originalController, ItemController itemController) {
-    this.scanner = new Scanner(System.in, "UTF-8");
-    this.itemController = itemController;
-    this.memberController = originalController;
-  }
+  private MemberRegistry memberRegistry;
+  private ItemRegistry itemRegistry;
 
   /**
    * Method that handles user inpur.
@@ -51,72 +40,59 @@ public class ItemView {
     System.out.println("3. Change category");
     System.out.println("4. Change cost per day");
     System.out.println("5. Back");
+
+    System.out.print("\nEnter your choice: ");
   }
 
-      switch (choice) {
-        case 1:
-          // Create Item.
-          System.out.println("Creating a new Item...\n");
-          System.out.print("Enter member ID of the owner: ");
-          String memberId = scanner.nextLine();
-          Member owner = memberController.searchMember(memberId);
+  public String promtMemberEmail() {
+    System.out.print("Enter the member's Email to change information: ");
+    String ownerEmail = scanner.nextLine();
+    memberRegistry.printMemberInfo(ownerEmail);
+    return ownerEmail;
+  }
 
-          if (owner != null) {
-            final ItemCategory category = getCategoryFromUserInput();
+  public void itemCreateInput() {
+    String email = promtMemberEmail();
+    Member owner = memberRegistry.searchMember(email);
+    if (owner != null) {
+      final ItemCategory category = getCategoryFromUserInput();
 
-            scanner.nextLine();
+      scanner.nextLine();
 
-            System.out.print("Enter Item Name: ");
-            final String name = scanner.nextLine();
+      System.out.print("Enter Item Name: ");
+      final String name = scanner.nextLine();
 
-            System.out.print("Enter Item Description: ");
-            final String description = scanner.nextLine();
+      System.out.print("Enter Item Description: ");
+      final String description = scanner.nextLine();
 
-            System.out.print("Enter Cost Per Day: ");
-            final int costPerDay = scanner.nextInt();
+      System.out.print("Enter Cost Per Day: ");
+      final int costPerDay = scanner.nextInt();
 
-            scanner.nextLine();
+      scanner.nextLine();
 
-            Item newItem = itemController.createItem(category, name, description, costPerDay, memberId);
-            System.out.println("Item ID: " + newItem.getItemId());
-            System.out.println("Item created successfully.");
-          } else {
-            System.out.println("Member not found with the specified memberId.");
-          }
-          break;
-        case 2:
-          List<String> allItems = itemController.getAllItems();
-
-          for (String items : allItems) {
-            // Display the member name as needed
-            System.out.println("Item: " + items);
-          }
-          break;
-        case 3:
-          // Deletes an item by item ID
-          System.out.print("Enter the items's ID to be deleted: ");
-          String deleteId = scanner.nextLine();
-          Item itemDelete = itemController.searchItem(deleteId);
-          Member itemowner = itemDelete.getOwner();
-          itemController.deleteItem(deleteId, itemowner.getMemberId());
-          System.out.println(itemDelete.getName() + "successfully deleted! ");
-          break;
-        case 4:
-          // Change Item information.
-          changeItemInfo();
-          break;
-        case 5:
-          viewItemInformation();
-          break;
-        case 6:
-          return;
-
-        default:
-          throw new IllegalArgumentException("Invalid choice.");
-
-      }
+      Item newItem = itemRegistry.createItem(category, name, description, costPerDay, email);
+      System.out.println("Item ID: " + newItem.getItemId());
+      System.out.println("Item created successfully.");
+    } else {
+      System.out.println("Member not found with the specified memberId.");
     }
+  }
 
+  public void displayItemList() {
+    List<String> allItems = itemRegistry.getAllItems();
+    for (String items : allItems) {
+      // Display the member name as needed
+      System.out.println("Item: " + items);
+    }
+  }
+
+  public void promtItemDelete() {
+    System.out.print("Enter the items's ID to be deleted: ");
+    String deleteId = scanner.nextLine();
+    Item itemDelete = itemRegistry.searchItem(deleteId);
+    Member itemowner = itemDelete.getOwner();
+    itemRegistry.deleteItem(deleteId, itemowner.getMemberId());
+    System.out.println(itemDelete.getName() + "successfully deleted! ");
   }
 
   public int getIntInput() {
@@ -127,6 +103,40 @@ public class ItemView {
       // Invalid input; do nothing, the loop will prompt again.
     }
     return input;
+  }
+
+  public String promtItemId() {
+    System.out.print("Enter the Item's ID to change information: ");
+    String changeId = scanner.nextLine();
+    itemRegistry.printItemInfo(changeId);
+    return changeId;
+  }
+
+
+  public void promptChangeName(String changeId) {
+    System.out.print("Enter new name: ");
+    String newName = scanner.nextLine();
+    itemRegistry.updateItemName(changeId, newName);
+  }
+
+  public void promptChangeDesc(String changeId) {
+    System.out.print("Enter new name: ");
+    String newName = scanner.nextLine();
+    itemRegistry.updateItemName(changeId, newName);
+  }
+  
+  public void promptChangeCategory(String changeId) {
+    ItemCategory newCategory = getCategoryFromUserInput();
+    System.out.println("New Category: " + newCategory);
+    itemRegistry.updateItemCategory(changeId, newCategory);
+    scanner.nextLine();
+  }
+
+  public void promptChangeCost(String changeId) {
+    System.out.print("Enter new cost per day: ");
+    int newCost = scanner.nextInt();
+    scanner.nextLine();
+    itemRegistry.updateCostPerDay(changeId, newCost);
   }
 
   /**
@@ -167,57 +177,6 @@ public class ItemView {
     }
   }
 
-  private void changeItemInfo() {
-    System.out.print("Enter the Item's ID to change information: ");
-    String changeId = scanner.nextLine();
-    itemController.printItemInfo(changeId);
-
-    while (true) {
-      System.out.println("\nWhat information would you like to change? ");
-      System.out.println("1. Change name");
-      System.out.println("2. Change description");
-      System.out.println("3. Change category");
-      System.out.println("4. Change cost per day");
-      System.out.println("5. Back");
-
-      System.out.print("\nEnter your choice: ");
-
-      int choice = getIntInput();
-      switch (choice) {
-        case 1:
-          // change the name
-          System.out.print("Enter new name: ");
-          String newName = scanner.nextLine();
-          itemController.updateItemName(changeId, newName);
-          break;
-        case 2:
-          // change the descritpion
-          System.out.print("Enter new description of item: ");
-          String newDesc = scanner.nextLine();
-          itemController.updateItemDesc(changeId, newDesc);
-          break;
-        case 3:
-          // change the Category
-          ItemCategory newCategory = getCategoryFromUserInput();
-          System.out.println("New Category: " + newCategory);
-          itemController.updateItemCategory(changeId, newCategory);
-          scanner.nextLine();
-          break;
-        case 4:
-          // Change cost per day of item.
-          System.out.print("Enter new cost per day: ");
-          int newCost = scanner.nextInt();
-          scanner.nextLine();
-          itemController.updateCostPerDay(changeId, newCost);
-          break;
-        case 5:
-          return;
-        default:
-          throw new IllegalArgumentException("Invalid input for changing item info.");
-      }
-    }
-  }
-
   /**
    * Method to view an item's information including its contracts (historical and
    * future).
@@ -225,11 +184,11 @@ public class ItemView {
   public void viewItemInformation() {
     System.out.print("Enter the Item's ID to view information: ");
     String itemId = scanner.nextLine();
-    Item item = itemController.searchItem(itemId);
+    Item item = itemRegistry.searchItem(itemId);
 
     if (item != null) {
       // Display item information
-      itemController.printItemInfo(itemId);
+      itemRegistry.printItemInfo(itemId);
 
       // Display item's contracts
       List<Contract> contracts = item.getContracts();
